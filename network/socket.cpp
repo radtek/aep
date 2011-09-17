994f4d6896f4116a0ae5e67f389c224a84d05aac
ruoxi <zanmato1984@gmail.com>
2011-09-17 17:25:27 +0800

    Editing socket stuff and network.vcproj

--- network/socket.cpp
+++ network/socket.cpp
@@ -1,152 +1,130 @@
-/*
- * NVIDIA_COPYRIGHT_BEGIN
- *
- * Copyright 1999-2011 by NVIDIA Corporation.  All rights reserved.  All
- * information contained herein is proprietary and confidential to NVIDIA
- * Corporation.  Any use, reproduction, or disclosure without the written
- * permission of NVIDIA Corporation is prohibited.
- *
- * NVIDIA_COPYRIGHT_END
- */
-
-#include "sockchip.h"
 #include "socket.h"
-#include <string.h>
 
 Socket::Socket()
-: ErrorHandler(NULL)
+:
+m_ErrorHandler(NULL)
 {
 }
 
-SockChipCommand Socket::RecvCommand()
+UINT08 Socket::Recv08()
 {
-    return (SockChipCommand)Recv16();
+    UINT08 data = 0;
+    Recv(&data, 1);
+    return data;
 }
 
-void Socket::SendCommand(SockChipCommand Command)
+void Socket::Send08(UINT08 data)
 {
-    Send16(Command);
+    Send(&data, 1);
 }
 
-NvU008 Socket::Recv08()
+UINT16 Socket::Recv16()
 {
-    NvU008 Data = 0;
-    Recv(&Data, 1);
-    return Data;
+    UINT08 bytes[2] = { 0, };
+    UINT16 data;
+    Recv(bytes, sizeof(bytes));
+    data  = bytes[0];
+    data |= bytes[1] << 8;
+    return data;
 }
 
-void Socket::Send08(NvU008 Data)
+void Socket::Send16(UINT16 data)
 {
-    Send(&Data, 1);
+    UINT08 bytes[2];
+    bytes[0] = (UINT08)(data & 0xFF);
+    bytes[1] = (UINT08)(data >> 8);
+    Send(bytes, sizeof(bytes));
 }
 
-NvU016 Socket::Recv16()
+UINT32 Socket::Recv32()
 {
-    NvU008 Bytes[2] = { 0, };
-    NvU016 Data;
-    Recv(Bytes, sizeof(Bytes));
-    Data  = Bytes[0];
-    Data |= Bytes[1] << 8;
-    return Data;
+    UINT08 bytes[4] = { 0, };
+    UINT32 data;
+    Recv(bytes, sizeof(bytes));
+    data  = bytes[0];
+    data |= bytes[1] << 8;
+    data |= bytes[2] << 16;
+    data |= bytes[3] << 24;
+    return data;
 }
 
-void Socket::Send16(NvU016 Data)
+void Socket::Send32(UINT32 data)
 {
-    NvU008 Bytes[2];
-    Bytes[0] = (NvU008)(Data & 0xFF);
-    Bytes[1] = (NvU008)(Data >> 8);
-    Send(Bytes, sizeof(Bytes));
+    UINT08 bytes[4];
+    bytes[0] = (UINT08)(data & 0xFF);
+    bytes[1] = (UINT08)((data >> 8) & 0xFF);
+    bytes[2] = (UINT08)((data >> 16) & 0xFF);
+    bytes[3] = (UINT08)(data >> 24);
+    Send(bytes, sizeof(bytes));
 }
 
-NvU032 Socket::Recv32()
+UINT64 Socket::Recv64()
 {
-    NvU008 Bytes[4] = { 0, };
-    NvU032 Data;
-    Recv(Bytes, sizeof(Bytes));
-    Data  = Bytes[0];
-    Data |= Bytes[1] << 8;
-    Data |= Bytes[2] << 16;
-    Data |= Bytes[3] << 24;
-    return Data;
+    UINT08 bytes[8] = { 0, };
+    UINT64 data;
+    Recv(bytes, sizeof(bytes));
+    data  = (UINT64)bytes[0];
+    data |= (UINT64)bytes[1] << 8;
+    data |= (UINT64)bytes[2] << 16;
+    data |= (UINT64)bytes[3] << 24;
+    data |= (UINT64)bytes[4] << 32;
+    data |= (UINT64)bytes[5] << 40;
+    data |= (UINT64)bytes[6] << 48;
+    data |= (UINT64)bytes[7] << 56;
+    return data;
 }
 
-void Socket::Send32(NvU032 Data)
+void Socket::Send64(UINT64 data)
 {
-    NvU008 Bytes[4];
-    Bytes[0] = (NvU008)(Data & 0xFF);
-    Bytes[1] = (NvU008)((Data >> 8) & 0xFF);
-    Bytes[2] = (NvU008)((Data >> 16) & 0xFF);
-    Bytes[3] = (NvU008)(Data >> 24);
-    Send(Bytes, sizeof(Bytes));
-}
-
-NvU064 Socket::Recv64()
-{
-    NvU008 Bytes[8] = { 0, };
-    NvU064 Data;
-    Recv(Bytes, sizeof(Bytes));
-    Data  = (NvU064)Bytes[0];
-    Data |= (NvU064)Bytes[1] << 8;
-    Data |= (NvU064)Bytes[2] << 16;
-    Data |= (NvU064)Bytes[3] << 24;
-    Data |= (NvU064)Bytes[4] << 32;
-    Data |= (NvU064)Bytes[5] << 40;
-    Data |= (NvU064)Bytes[6] << 48;
-    Data |= (NvU064)Bytes[7] << 56;
-    return Data;
-}
-
-void Socket::Send64(NvU064 Data)
-{
-    NvU008 Bytes[8];
-    Bytes[0] = (NvU008)(Data & 0xFF);
-    Bytes[1] = (NvU008)((Data >> 8) & 0xFF);
-    Bytes[2] = (NvU008)((Data >> 16) & 0xFF);
-    Bytes[3] = (NvU008)((Data >> 24) & 0xFF);
-    Bytes[4] = (NvU008)((Data >> 32) & 0xFF);
-    Bytes[5] = (NvU008)((Data >> 40) & 0xFF);
-    Bytes[6] = (NvU008)((Data >> 48) & 0xFF);
-    Bytes[7] = (NvU008)(Data >> 56);
-    Send(Bytes, sizeof(Bytes));
+    UINT08 bytes[8];
+    bytes[0] = (UINT08)(data & 0xFF);
+    bytes[1] = (UINT08)((data >> 8) & 0xFF);
+    bytes[2] = (UINT08)((data >> 16) & 0xFF);
+    bytes[3] = (UINT08)((data >> 24) & 0xFF);
+    bytes[4] = (UINT08)((data >> 32) & 0xFF);
+    bytes[5] = (UINT08)((data >> 40) & 0xFF);
+    bytes[6] = (UINT08)((data >> 48) & 0xFF);
+    bytes[7] = (UINT08)(data >> 56);
+    Send(bytes, sizeof(bytes));
 }
 
 double Socket::RecvDouble()
 {
-    NvU064 Data = Recv64();
-    return *(double *)&Data;
+    UINT64 data = Recv64();
+    return *(double *)&data;
 }
 
-void Socket::SendDouble(double Data)
+void Socket::SendDouble(double data)
 {
-    Send64(*(NvU064 *)&Data);
+    Send64(*(UINT64 *)&data);
 }
 
 // We could use null-terminated strings, but sending the length is more
 // efficient for the receiver.
 string Socket::RecvString()
 {
-    NvU032 Length = Recv32();
-    string Str;
-    Str.resize(Length);
-    Recv(&Str[0], Length);
-    return Str;
+    UINT32 length = Recv32();
+    string str;
+    str.resize(length);
+    Recv(&str[0], length);
+    return str;
 }
 
-void Socket::SendString(const char *Str)
+void Socket::SendString(const char *str)
 {
-    NvU032 Length = (NvU032)strlen(Str);
-    Send32(Length);
-    Send(Str, Length);
+    UINT32 length = (UINT32)strlen(str);
+    Send32(length);
+    Send(str, length);
 }
 
-void Socket::InstallErrorHandler(ErrorHandlerFunc Function, void *Args)
+void Socket::InstallErrorHandler(ErrorHandlerFunc func, void *args)
 {
-    ErrorHandler = Function;
-    ErrorHandlerArgs = Args;
+    m_ErrorHandler = func;
+    m_ErrorHandlerArgs = args;
 }
 
 void Socket::CallErrorHandler()
 {
-    if (ErrorHandler)
-        ErrorHandler(ErrorHandlerArgs);
+    if (m_ErrorHandler)
+        m_ErrorHandler(m_ErrorHandlerArgs);
 }
