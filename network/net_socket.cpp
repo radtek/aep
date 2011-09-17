e5e5552d741a30adde6506bac8d2f294faf90c6a
ruoxi <zanmato1984@gmail.com>
2011-09-17 19:18:58 +0800

    Editing net_socket.cpp and removing ReadMe.txt.

--- network/net_socket.cpp
+++ network/net_socket.cpp
@@ -1,10 +1,10 @@
+#include "net_socket.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
 #include <string.h>
-
-#include "net_socket.h"
 
 #define socklen_t int
 
@@ -80,7 +80,7 @@ void NetSocket::Listen(int port)
 
 void NetSocket::Connect(const char *hostname, int port)
 {   
-    SOCKADDR_IN serverSIN;
+    SOCKADDR_IN serverSockIn;
     struct hostent *host;
     
     host = gethostbyname(hostname);
@@ -90,12 +90,12 @@ void NetSocket::Connect(const char *hostname, int port)
         Error("Could not find ip address for %s!", hostname);
     }
 
-    serverSIN.sin_family = AF_INET;
-    serverSIN.sin_port = htons(port);
-    memcpy(&serverSIN.sin_addr, host->h_addr, host->h_length);
+    serverSockIn.sin_family = AF_INET;
+    serverSockIn.sin_port = htons(port);
+    memcpy(&serverSockIn.sin_addr, host->h_addr, host->h_length);
     int sock_err = connect(m_Socket,
-                           (struct sockaddr *)&serverSIN,
-                           sizeof(serverSIN));
+                           (struct sockaddr *)&serverSockIn,
+                           sizeof(serverSockIn));
     if (sock_err == SOCKET_ERROR)
     {
         Error("Could not connect to remote server!");
@@ -104,32 +104,32 @@ void NetSocket::Connect(const char *hostname, int port)
 
 int NetSocket::GetPortNumber()
 {
-    SOCKADDR_IN serverSIN = {0};
-    socklen_t namelen = sizeof(serverSIN);
+    SOCKADDR_IN serverSockIn = {0};
+    socklen_t namelen = sizeof(serverSockIn);
 
     int sock_err = getsockname(m_Socket,
-                               (struct sockaddr *)&serverSIN,
+                               (struct sockaddr *)&serverSockIn,
                                &namelen);
     if (sock_err == SOCKET_ERROR)
     {
         Error("Could not get port number!");
     }
 
-    return ntohs(serverSIN.sin_port);
+    return ntohs(serverSockIn.sin_port);
 }
 
 void NetSocket::WaitForConnection()
 {
-    SOCKADDR_IN ClientSIN;
-    SOCKET ClientSock;
+    SOCKADDR_IN clientSockIn;
+    SOCKET clientSock;
 
-    int ClientSINLen;
+    int clientSockInLen;
 
-    ClientSINLen = sizeof(ClientSIN);
-    ClientSock = accept(m_Socket,
-                        (struct sockaddr *)&ClientSIN,
-                        &ClientSINLen);
-    if (ClientSock == INVALID_SOCKET)
+    clientSockInLen = sizeof(clientSockIn);
+    clientSock = accept(m_Socket,
+                        (struct sockaddr *)&clientSockIn,
+                        &clientSockInLen);
+    if (clientSock == INVALID_SOCKET)
     {
         Error("Could not wait for a connection!");
         return;
@@ -137,19 +137,19 @@ void NetSocket::WaitForConnection()
 
     // Throw away the original server socket
     closesocket(m_Socket);
-    m_Socket = ClientSock;
+    m_Socket = clientSock;
 }
 
-void NetSocket::Recv(void *buf, size_t Size)
+void NetSocket::Recv(void *buf, size_t size)
 {
     size_t total_recv_len = 0;
-    while (total_recv_len < Size)
+    while (total_recv_len < size)
     {
-        int ret_len = recv(m_Socket, (char *)buf+total_recv_len, int(Size-total_recv_len), MSG_WAITALL);
+        int ret_len = recv(m_Socket, (char *)buf+total_recv_len, int(size-total_recv_len), MSG_WAITALL);
         if (ret_len == SOCKET_ERROR)
         {
             CallErrorHandler();
-            Error("Could not receive %d bytes!", Size);
+            Error("Could not receive %d bytes!", size);
         }
         else if (ret_len == 0)
         {
@@ -160,13 +160,13 @@ void NetSocket::Recv(void *buf, size_t Size)
     }
 
 #if VERBOSE_SEND_RECV
-    printf("[net] Recieved %d/%lu bytes:", total_recv_len, Size);
+    printf("[net] Recieved %d/%lu bytes:", total_recv_len, size);
 
-    if (Size == 1)
+    if (size == 1)
     {
         printf("\n[net]   %02x", *(UINT08 *)buf);
     }
-    else if (Size == 2)
+    else if (size == 2)
     {
         printf("\n[net]   %04x", UINT16(*(UINT16 *)buf));
     }
@@ -174,7 +174,7 @@ void NetSocket::Recv(void *buf, size_t Size)
     {
         UINT32 *ptr = (UINT32 *) buf;
 
-        for (size_t ii = 0; ii < Size / 4; ii++)
+        for (size_t ii = 0; ii < size / 4; ii++)
         {
             if ((ii & 7) == 0) {
                 printf("\n[net]  ");
@@ -187,28 +187,28 @@ void NetSocket::Recv(void *buf, size_t Size)
 #endif
 }
 
-void NetSocket::Send(const void *buf, size_t Size)
+void NetSocket::Send(const void *buf, size_t size)
 {
     size_t total_send_len = 0;
-    while (total_send_len < Size)
+    while (total_send_len < size)
     {
-        int ret_len = send(m_Socket, (const char *)buf+total_send_len, int(Size-total_send_len), MSG_WAITALL);
+        int ret_len = send(m_Socket, (const char *)buf+total_send_len, int(size-total_send_len), MSG_WAITALL);
         if (ret_len == SOCKET_ERROR)
         {
             CallErrorHandler();
-            Error("Could not send %d bytes!", Size);
+            Error("Could not send %d bytes!", size);
         }
         total_send_len += ret_len;
     }
 
 #if VERBOSE_SEND_RECV
-    printf("[net] Sent %d/%lu bytes:", total_send_len, Size);
+    printf("[net] Sent %d/%lu bytes:", total_send_len, size);
 
-    if (Size == 1)
+    if (size == 1)
     {
         printf("\n[net]   %02x", *(const UINT08 *)buf);
     }
-    else if (Size == 2)
+    else if (size == 2)
     {
         printf("\n[net]   %04x", UINT16(*(const UINT16 *)buf));
     }
@@ -216,7 +216,7 @@ void NetSocket::Send(const void *buf, size_t Size)
     {
         const UINT32 *ptr = (const UINT32 *) buf;
 
-        for (size_t ii = 0; ii < Size / 4; ii++)
+        for (size_t ii = 0; ii < size / 4; ii++)
         {
             if ((ii & 7) == 0) {
                 printf("\n[net]  ");
