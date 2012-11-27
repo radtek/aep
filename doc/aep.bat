629a878d8cff6107fc65a48af628457febe2ba11
ruoxi <zanmato1984@gmail.com>
2012-11-28 00:05:02 +0800

    Fixing hhc.exe location.

--- doc/aep.bat
+++ doc/aep.bat
@@ -8,7 +8,7 @@ call utf82gbk.bat index.hhk
 call utf82gbk.bat *.html
 call html-utf82gbk *.html
 
-"C:\Program Files\HTML Help Workshop\hhc.exe" index.hhp
+"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" index.hhp
 
 if exist index.chm copy index.chm ..\≥Ã–Ú‘± ÷≤·.chm
 
